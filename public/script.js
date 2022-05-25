87b98ba35090fe110634f87ab9c66c23eb78547f
E-tim <eesuolaayot@gmail.com>
2022-05-25 22:24:48 +0800

    first commit

---
+++ public/script.js
@@ -0,0 +1,23 @@
+
+
+// let par = document.getElementById('mypar');
+
+// let arr = [par];
+
+// arr.map( ar => {
+//     if (ar.value < 0) {
+//         ar.style.color = 'red' ;
+//     } else {
+//         ar.style.color = 'green'
+//     }
+// } )
+
+// const myFun = ()=> {
+//     if (par.value < 0) {
+//         par.style.color = 'red'
+//     } else {
+//         par.style.color = 'green'
+//     }
+// }
+// myFun()
+
---
+++ server.js
@@ -0,0 +1,51 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const axios = require('axios');
+
+app.use(express.urlencoded({extended: false}))
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+// connecting public to server js
+app.use(express.static(path.join(__dirname, "public")))
+
+function getWeather(req, res, next) {
+    req.isWeather = false;
+    next();
+}
+
+
+
+app.get('/', getWeather, (req, res)=> {
+
+
+    // if (req.isWeather) {
+    //     res.send('It is rainning , come another day')
+    // } else {
+    //     res.send('You are welcome to this page')
+    // }
+
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').
+    then( results => {
+        res.render("index", {
+            data: results.data
+        })
+    } )
+    
+})
+
+app.post('/users', (req, res)=> {
+    if (req.body.name === 'tim' && req.body.age === 22) {
+        res.send('This is correct');
+    } else {
+        res.send('This is wrong')
+    }
+})
+
+
+
+
+app.listen( process.env.PORT || 3000);
